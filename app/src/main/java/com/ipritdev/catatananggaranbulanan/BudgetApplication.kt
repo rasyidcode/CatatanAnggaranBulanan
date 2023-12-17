15c2e6e46efa9b3b6df9a0a98e6960bede424682
rasyidcode <rasyidcode7@gmail.com>
2023-12-18 01:45:21 +0700

    initial commit

---
+++ app/src/main/java/com/ipritdev/catatananggaranbulanan/BudgetApplication.kt
@@ -0,0 +1,6 @@
+package com.ipritdev.catatananggaranbulanan
+
+import android.app.Application
+
+class BudgetApplication : Application() {
+}
---
+++ app/src/main/java/com/ipritdev/catatananggaranbulanan/BudgetFragment.kt
@@ -0,0 +1,31 @@
+package com.ipritdev.catatananggaranbulanan
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ipritdev.catatananggaranbulanan.databinding.FragmentBudgetBinding
+
+class BudgetFragment : Fragment() {
+
+    private var _binding: FragmentBudgetBinding? = null
+
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBudgetBinding.inflate(inflater, container, false)
+
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
